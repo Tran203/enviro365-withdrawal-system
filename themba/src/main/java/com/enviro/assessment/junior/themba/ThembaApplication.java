985e66f9571ee05c111fcc78628b7a748c00a247
Tran203 <thembaMabe@joburg.org.za>
2026-06-10 23:57:27 +0200

    Created Project Files

---
+++ themba/src/main/java/com/enviro/assessment/junior/themba/ThembaApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.junior.themba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThembaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThembaApplication.class, args);
+	}
+
+}
